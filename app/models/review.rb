b6f46972cf71b8463a8f40b004ee5803de66c502
Vikram Swamy <vswamy@gmail.com>
2015-03-16 21:59:58 -0400

    Updated review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,5 @@ class Review < ActiveRecord::Base
   validates_presence_of :rating, :body
   validates_numericality_of :rating, only_integer: :true,
     :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5
-  # validates_uniqueness_of :user_id, scope: :video_id
+  validates_uniqueness_of :user_id, scope: :video_id
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -10,6 +10,6 @@ describe Review do
   it { should validate_numericality_of(:rating).only_integer }
   it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
   it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
-  # it { should validate_uniqueness_of(:user_id).scoped_to(:video_id) }
+  it { should validate_uniqueness_of(:user_id).scoped_to(:video_id) }
 
 end
