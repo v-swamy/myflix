60d53f1ca7fcae48cd53d452282283ab8369c1f6
Vikram Swamy <vswamy@gmail.com>
2015-03-27 10:27:41 -0400

    Week 3 first update

--- spec/controllers/queue_items_controller_spec.rb
+++ spec/controllers/queue_items_controller_spec.rb
@@ -94,6 +94,13 @@ describe QueueItemsController do
         expect(user.queue_items.count).to eq(1)
       end
 
+      it "normalizes the remaining queue items" do
+        queue_item1 = Fabricate(:queue_item, user: user, position: 1)
+        queue_item2 = Fabricate(:queue_item, user: user, position: 2)
+        delete :destroy, id: queue_item1.id
+        expect(QueueItem.first.position).to eq(1)
+      end
+
       it "does not delete the queue item if not in current user's queue" do
         user2 = Fabricate(:user)
         queue_item = Fabricate(:queue_item, user: user2)
@@ -107,6 +114,78 @@ describe QueueItemsController do
       it "redirects to the sign in page for unauthenticated users" do
         delete :destroy, id: 1
         expect(response).to redirect_to sign_in_path
+      end
+    end
+  end
+
+  describe "POST update_queue" do
+    context "with valid inputs" do
+
+      let(:user) { Fabricate(:user) }
+      let(:video) { Fabricate (:video) }
+      let(:queue_item1) { Fabricate(:queue_item, user: user, position: 1, video: video) }
+      let(:queue_item2) { Fabricate(:queue_item, user: user, position: 2, video: video) }
+
+      before { session[:user_id] = user.id }
+
+      it "redirects to the my_queue page" do
+        post :update_queue, queue_items: [{id: queue_item1.id, position: 2}, {id: queue_item2.id, position: 1}]
+        expect(response).to redirect_to my_queue_path
+      end
+
+      it "reorders the queue items" do
+        post :update_queue, queue_items: [{id: queue_item1.id, position: 2}, {id: queue_item2.id, position: 1}]
+        expect(user.queue_items).to eq([queue_item2, queue_item1])
+      end
+
+      it "normalizes the position numbers" do
+        post :update_queue, queue_items: [{id: queue_item1.id, position: 3}, {id: queue_item2.id, position: 2}]
+        expect(user.queue_items.map(&:position)).to eq([1, 2])
+      end
+    end
+
+    context "with invalid inputs" do
+
+      let(:user) { Fabricate(:user) }
+      let(:video) { Fabricate (:video) }
+      let(:queue_item1) { Fabricate(:queue_item, user: user, position: 1, video: video) }
+      let(:queue_item2) { Fabricate(:queue_item, user: user, position: 2, video: video) }
+
+      before { session[:user_id] = user.id }
+
+      it "redirects to the my_queue page" do
+        post :update_queue, queue_items: [{id: queue_item1.id, position: 3.4}, {id: queue_item2.id, position: 2}]
+        expect(response).to redirect_to my_queue_path      
+      end
+
+      it "sets the flash error message" do
+        post :update_queue, queue_items: [{id: queue_item1.id, position: 3.4}, {id: queue_item2.id, position: 2}]
+        expect(flash[:danger]).to be_present     
+      end
+
+      it "does not change the queue items" do
+          post :update_queue, queue_items: [{id: queue_item1.id, position: 3}, {id: queue_item2.id, position: 2.1}]
+          expect(queue_item1.reload.position).to eq(1)   
+      end
+    end
+
+    context "with unauthenticated users" do
+      it "redirects to the sign in path" do
+        post :update_queue, queue_items: [{id: 2, position: 3}, {id: 3, position: 4}]
+        expect(response).to redirect_to sign_in_path
+      end
+    end
+
+    context "with queue items that do not belong to current user" do
+      it "does not change the queue items" do
+        user = Fabricate(:user)
+        session[:user_id] = user.id
+        user2 = Fabricate(:user)
+        video = Fabricate(:video)
+        queue_item1 = Fabricate(:queue_item, user: user2, position: 1, video: video)
+        queue_item2 = Fabricate(:queue_item, user: user, position: 2, video: video)
+        post :update_queue, queue_items: [{id: queue_item1.id, position: 2}, {id: queue_item2.id, position: 1}]
+        expect(queue_item1.reload.position).to eq(1)
       end
     end
   end
--- spec/models/queue_item_spec.rb
+++ spec/models/queue_item_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe QueueItem do
   it { should belong_to(:user) }
   it { should belong_to(:video) }
+  it { should validate_numericality_of(:position) }
 
   it { is_expected.to delegate_method(:video_title).to(:video).as(:title) }
   it { is_expected.to delegate_method(:category).to(:video) }
@@ -24,4 +25,31 @@ describe QueueItem do
       expect(queue_item.rating).to eq(nil)
     end
   end
+
+  describe "#=rating" do
+    it "changes the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, video: video, user: user, rating: 2)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      queue_item.rating = 4
+      expect(Review.first.rating).to eq(4)
+    end
+    it "clears the rating of the review if the review is present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      review = Fabricate(:review, video: video, user: user, rating: 2)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      queue_item.rating = nil
+      expect(Review.first.rating).to be_nil
+    end
+    it "creates a new review with the rating if no review present" do
+      video = Fabricate(:video)
+      user = Fabricate(:user)
+      queue_item = Fabricate(:queue_item, video: video, user: user)
+      queue_item.rating = 3
+      expect(Review.first.rating).to eq(3)
+    end
+  end
+
 end
