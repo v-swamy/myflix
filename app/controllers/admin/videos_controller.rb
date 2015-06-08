b7940fa2ee097c4253e1aff4b85482d9d8e00460
Vikram Swamy <vswamy@gmail.com>
2015-06-08 10:52:25 -0400

    added admin video upload controller, view, tests

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -6,7 +6,22 @@ class Admin::VideosController < ApplicationController
     @video = Video.new
   end
 
+  def create
+    @video = Video.create(video_params)
+    if @video.save
+      flash[:success] = "You have successfully added the video '#{@video.title}'."
+      redirect_to new_admin_video_path
+    else
+      flash[:danger] = "You cannot add this video.  Please check the errors and resubmit."
+      render :new
+    end
+  end
+
   private
+
+  def video_params
+    params.require(:video).permit(:title, :description, :category_id)
+  end
 
   def require_admin
     if !current_user.admin?
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -6,6 +6,14 @@ shared_examples "requires sign in" do
   end
 end
 
+shared_examples "requires admin" do
+  it "redirects a regular user to the home path" do
+    set_current_user
+    action
+    expect(response).to redirect_to home_path
+  end
+end
+
 shared_examples "tokenable" do
   it "generates a random token when the user is created" do
     expect(object.token).to be_present
