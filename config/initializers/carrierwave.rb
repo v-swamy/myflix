d2688a4f2ce42e3206059efbf126749594f72c7b
Vikram Swamy <vswamy@gmail.com>
2015-06-10 17:54:45 -0400

    edited carrierwave initializer for host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,11 @@ CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.staging?
     config.storage = :aws
     config.aws_bucket = ENV['S3_BUCKET_NAME']
+    config.asset_host = 'http://vswamy-myflix-staging.s3-website-us-east-1.amazonaws.com'
     config.aws_credentials = {
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      # region: ENV['AWS_REGION'] 
+      region: ENV['AWS_REGION'] 
     }
 
   else
