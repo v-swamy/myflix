bfd2d50d54f0f97eee8e59238ba14b9973225a85
Vikram Swamy <vswamy@gmail.com>
2015-06-10 15:33:39 -0400

    edited carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     config.aws_credentials = {
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'] 
+      # region: ENV['AWS_REGION'] 
     }
 
   else
