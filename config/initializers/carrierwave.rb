5e77154892c1b69504e2f9b47d48bbcd7f4946b5
Vikram Swamy <vswamy@gmail.com>
2015-06-10 20:51:00 -0400

    yet another carrierwave.rb fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,8 @@ CarrierWave.configure do |config|
     config.aws_credentials = {
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION'],
-      config: AWS.config({
-        s3_endpoint: 's3-us-east-1.amazonaws.com'
-      })
+      # region: ENV['AWS_REGION'],
+      config: AWS.config({s3_endpoint: 's3-us-east-1.amazonaws.com'})
     }
 
   else
